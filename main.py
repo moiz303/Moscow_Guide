61ee92ce02c7975e810a9b1ec4ab416740c322f5
moiz303 <moiz.izraylit@yandex.ru>
2024-04-21 17:23:54 +0300

    Unversioned Files

--- main.py
+++ main.py
@@ -1,95 +1,139 @@
-import json
-import shutil
-import os
-import osmnx
-import requests
-from requests.adapters import HTTPAdapter
-from urllib3 import Retry
+import pyttsx3
+import wikipedia
+
+from log_in import search
+
+from flask import Flask, render_template, redirect, url_for
+from flask_login import LoginManager, login_user, login_required, logout_user
+
+from data import db_session
+from data.login_form import LoginForm
+from data.users import User
+from data.register import RegisterForm
+from data.make_req import Reqest
+from data.text_to_speech import TTS
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
-def refresh_map(map_ll):
+def TextSpeech(text_speech: str):
     """
-    Перезагрузка карты
+    Озвучиваем текст
     """
-    map_params = {
-        "ll": f'{map_ll[0]},{map_ll[1]}',
-        "l": 'map',
-        'z': 16,
-    }
-    response = make_request('https://static-maps.yandex.ru/1.x/', params=map_params)
-    if not response:
-        print('Ошибка: не могу получить карту')
-        return
-    with open('templates/tmp.jpg', mode='wb') as tmp:
-        tmp.write(response.content)
+    tts = pyttsx3.init()
+    tts.setProperty('voice', 'ru')
+    tts.say(text_speech)
+    tts.runAndWait()
 
 
-def search(name):
+@login_manager.user_loader
+def load_user(user_id):
     """
-    Поиск по карте
+    Создаём сессию в базе
     """
-    x, y = geo_locate(name)
-    if x == -1 or y == -1:
-        return
-    map_ll = [x, y]
-    refresh_map(map_ll)
-    shutil.rmtree(os.path.abspath('cache'))
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 
-def geo_locate(name):
+@app.route('/login', methods=['GET', 'POST'])
+def login():
     """
-    Ищем локацию по названию OSM-кой
-    Проверка на расположение с помощью Яндекс.карт
+    Логинимся
     """
-    try:
-        coords = osmnx.geocode(name)
-        params = {
-            'apikey': '40d1649f-0493-4b70-98ba-98533de7710b',
-            'geocode': str(coords[1]) + ' ' + str(coords[0]),
-            'format': 'json'
-        }
-        response = make_request('http://geocode-maps.yandex.ru/1.x/', params=params)
-    except osmnx._errors.InsufficientResponseError:
-        print(f'Ошибка: не могу получить место с названием "{name}"')
-        return -1, -1
-
-    with open('result.json', 'w', encoding='utf8') as file:
-        text = response.json()
-        file.write(json.dumps(text, ensure_ascii=False))
-
-    geo_objects = response.json()['response']["GeoObjectCollection"]["featureMember"]
-    if not geo_objects:
-        print('Ошибка: не могу получить место')
-        return -1, -1
-
-    correct_geo_objects = []
-
-    for geo_object in geo_objects:
-        try:
-            if (geo_object["GeoObject"]['metaDataProperty']['GeocoderMetaData']['AddressDetails']['Country']
-            ['AdministrativeArea']['AdministrativeAreaName'] == 'Москва'):
-                correct_geo_objects.append(geo_object)
-        except KeyError:
-            pass
-
-    if not correct_geo_objects:
-        print('Ошибка: место находится не в Москве')
-        return -1, -1
-
-    return [coords[1], coords[0]]
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html', message="Неправильный логин или пароль", form=form)
+    return render_template('login.html', title='Авторизация', form=form)
 
 
-def make_request(*args, **kwargs):
+@app.route("/", methods=['GET', 'POST'])
+def index():
     """
-    Собираем запрос
+    Сама, собственно, база
     """
-    session = requests.Session()
-    retry = Retry(total=100, connect=5, backoff_factor=0.5)
-    adapter = HTTPAdapter(max_retries=retry)
-    session.mount('http://', adapter)
-    session.mount('https://', adapter)
-    return session.get(*args, **kwargs)
+    form = Reqest()
+    if form.validate_on_submit():
+        return redirect(url_for(".give_info", request=form.req.data))
+    return render_template("index.html", form=form)
+
+
+@app.route("/give_info/<request>", methods=['GET', 'POST'])
+def give_info(request):
+    """
+    Получаем информацию с Википедии и постим её с картой
+    """
+    wikipedia.set_lang('ru')
+    form = TTS()
+    search(request)
+    info = wikipedia.summary(request)
+    src = f"{url_for('static', filename='img/tmp.jpg')}"
+    if form.is_submitted():
+        TextSpeech(info)
+    return render_template("give_info.html", text=info, form=form, src=src)
+
+
+@app.route("/find_cat_8")
+def cat_8():
+    """
+    Пасхалочка на найди кота 8
+    """
+    return render_template("cat8.html")
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    """
+    Выходим из учётной записи
+    """
+    logout_user()
+    return redirect("/")
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    """
+    Регистрация
+    """
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация', form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация', form=form,
+                                   message="Пользователь уже существует")
+        user = User(
+            name=form.name.data,
+            surname=form.surname.data,
+            age=form.age.data,
+            position=form.position.data,
+            email=form.email.data,
+            speciality=form.speciality.data,
+            address=form.address.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+def log_in():
+    db_session.global_init("db/Users.sqlite3")
+
+    app.run()
 
 
 if __name__ == '__main__':
-    search('Нескучный сад')
+    log_in()
